9cd3f9a14796d962fb4e415da5e9cd27803b8c16
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-04-21 21:39:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search a node in BST/search-a-node-in-bst.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public boolean search(Node root, int key) {
+        // code here
+        while (root != null) {
+            if (root.data == key) return true;
+            else if (root.data < key) root = root.right;
+            else root = root.left;
+        }
+        
+        return false;
+    }
+}
