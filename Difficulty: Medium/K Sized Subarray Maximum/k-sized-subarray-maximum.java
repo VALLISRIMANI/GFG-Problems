dc9e561e179ae72152871299109fa7e6a5f48b90
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-05-05 10:54:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) {
+                deque.removeLast();
+            }
+            
+            deque.addLast(i);
+        }
+
+        result.add(arr[deque.peekFirst()]);
+
+        for (int i = k; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.removeFirst();
+            }
+
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) {
+                deque.removeLast();
+            }
+
+            deque.addLast(i);
+
+            result.add(arr[deque.peekFirst()]);
+        }
+
+        return result;
+    }
+}
