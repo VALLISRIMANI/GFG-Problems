9a1e050dd69138e32622acbdb7e90921673f7215
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-05-13 11:06:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int kBitFlips(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int flip = 0, flipCount = 0;
+        int[] isFlipped = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (i >= k) {
+                flip ^= isFlipped[i - k];
+            }
+            
+            if ((arr[i] ^ flip) == 0) {
+                
+                if (i + k > n) {
+                    return -1;
+                }
+                
+                isFlipped[i] = 1;
+                flip ^= 1;
+                flipCount++;
+            }
+        }
+        
+        return flipCount;
+    }
+}
