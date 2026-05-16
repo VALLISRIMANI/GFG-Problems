1967a1a79743344aace0b234c05af232dc2b0eb3
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-05-16 22:40:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int lis(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        int maxLength = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+}
