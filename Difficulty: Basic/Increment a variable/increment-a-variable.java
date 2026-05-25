e9596113e8bf887f8e48313782c027b6e4ffb995
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-05-25 13:30:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Increment a variable/increment-a-variable.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int updateVar(int a) {
+        // code here
+        for (int i = 1; i <= 10; i++) {
+            a++;
+        }
+        
+        return a;
+    }
+}
