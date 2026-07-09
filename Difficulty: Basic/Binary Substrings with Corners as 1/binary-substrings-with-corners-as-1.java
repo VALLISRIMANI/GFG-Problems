f50fc60e6e0fc89a7881195ae2523bfbce1a3402
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-07-09 21:25:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Substrings with Corners as 1/binary-substrings-with-corners-as-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int binarySubstring(String s) {
+        // code here
+        int ones = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+        
+        return ones * (ones - 1) / 2;
+    }
+}
