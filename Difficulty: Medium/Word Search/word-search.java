0d8c7df8b85c7bbd7cc512d6faa12fbc1f31bb25
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-07-14 15:31:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Search/word-search.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[][] dir = {
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
+    
+    public boolean isWordExist(char[][] mat, String word) {
+        // Code here
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(mat, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean dfs(char[][] mat, String word, int i, int j, int idx) {
+        if (idx == word.length()) return true;
+        
+        if (i < 0 || j < 0 || i >= mat.length || j >= mat[0].length) return false;
+        
+        if (mat[i][j] != word.charAt(idx)) return false;
+        
+        char temp = mat[i][j];
+        mat[i][j] = '#';
+        
+        for (int[] d : dir) {
+            int r = i + d[0];
+            int c = j + d[1];
+            
+            if (dfs(mat, word, r, c, idx + 1)) {
+                return true;
+            }
+        }
+        
+        mat[i][j] = temp;
+        
+        return false;
+    }
+}
