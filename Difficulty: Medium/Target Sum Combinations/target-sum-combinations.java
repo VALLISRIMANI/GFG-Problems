9269a8e7e654818d53c2432e0c3b8fedf2174fbf
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-04 22:37:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Target Sum Combinations/target-sum-combinations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> targetSumComb(int[] arr, int target) {
+        // code here
+        Arrays.sort(arr);
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        backtrack(arr, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private static void backtrack(int[] arr, int target, int start, ArrayList<Integer> curr, ArrayList<ArrayList<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i = start; i < arr.length; i++) {
+
+            if (arr[i] > target) {
+                break;
+            }
+
+            curr.add(arr[i]);
+            backtrack(arr, target - arr[i], i, curr, result);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
