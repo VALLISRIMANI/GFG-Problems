593f1c1fe29913c476a29f9cf0c53254e53cb01e
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-17 14:40:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n = mat.length, m = mat[0].length;
+
+        int row = 0, col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (mat[row][col] == x) return true;
+            else if (mat[row][col] > x) col--;
+            else row++;
+        }
+
+        return false;
+    }
+}
