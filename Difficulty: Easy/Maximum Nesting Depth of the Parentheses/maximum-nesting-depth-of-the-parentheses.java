7967c518e32a78333c877b09adc03b833f62b524
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-25 12:23:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Nesting Depth of the Parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int maxDepth(String s) {
+        // code here
+        int depth = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                depth++;
+                max = Math.max(max, depth);
+            } else if (ch == ')') {
+                depth--;
+            }
+        }
+
+        return max;
+    }
+}
