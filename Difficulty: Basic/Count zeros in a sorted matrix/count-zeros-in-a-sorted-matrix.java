c1f331924206f9f4a1d8f29d33459aa67177590f
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-28 14:45:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count zeros in a sorted matrix/count-zeros-in-a-sorted-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countZeros(int[][] mat) {
+        // code here
+        int count = 0;
+        
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
