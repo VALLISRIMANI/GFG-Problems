5cc5733b9e30bb6012317d25afc13898797301b7
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-08-28 14:24:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Strings/c-strings.java
@@ -0,0 +1,9 @@
+class Sol {
+    String conCat(String s1, String s2) {
+        // code here.
+        StringBuilder sb = new StringBuilder(s1);
+        sb.append(s2);
+        
+        return sb.toString();
+    }
+}
