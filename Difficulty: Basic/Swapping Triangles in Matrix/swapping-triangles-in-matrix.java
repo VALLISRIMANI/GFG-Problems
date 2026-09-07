4345d2d7a5ad719d51033444d1cdf5a4e9ea76ca
VALLISRIMANI GUDAPATI <manigudapati61@gmail.com>
2026-09-07 21:40:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swapping Triangles in Matrix/swapping-triangles-in-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    void swapTriangle(int mat[][]) {
+        // code here
+        int n = mat.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+               int temp = mat[i][j];
+               mat[i][j] = mat[j][i];
+               mat[j][i] = temp;
+            }
+        }
+    }
+};
